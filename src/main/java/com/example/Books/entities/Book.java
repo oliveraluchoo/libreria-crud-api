afd2298d9f163137d91241b2cc8b138662493207
oliveraLuciano <lucianoolivera756@gmail.com>
2025-02-08 17:19:23 -0300

    initial commit

---
+++ src/main/java/com/example/Books/entities/Book.java
@@ -0,0 +1,25 @@
+package com.example.Books.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private Integer aniopublicacion;
+    private String genero;
+
+
+}
---
+++ src/main/java/com/example/Books/services/BookService.java
@@ -0,0 +1,57 @@
+package com.example.Books.services;
+
+
+import com.example.Books.entities.Book;
+import com.example.Books.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService implements IBookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+        bookRepository.findById(id).orElseThrow( () -> new IllegalArgumentException("No hay libro con id: " + id));
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public Book updateBook(Long id, Book newBook) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No hay libro con id: " + id));
+        if (newBook.getTitulo() != null){
+            book.setTitulo(newBook.getTitulo());
+        }
+        if(newBook.getIsbn() != null){
+            book.setIsbn(newBook.getIsbn());
+        }
+        if(newBook.getGenero() != null){
+            book.setGenero(newBook.getGenero());
+        }
+        if(newBook.getAutor() != null){
+            book.setAutor(newBook.getAutor());
+        }
+        if(newBook.getAniopublicacion() != null){
+            book.setAniopublicacion(newBook.getAniopublicacion());
+        }
+        book = bookRepository.save(book);
+        return book;
+    }
+
+    public Book findBookById(Long id){
+        return bookRepository.findBookById(id);
+    }
+}
