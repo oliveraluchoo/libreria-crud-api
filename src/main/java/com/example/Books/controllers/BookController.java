65133e3646802fed37e7627b598bd8b982fb39c2
oliveraLuciano <lucianoolivera756@gmail.com>
2025-02-16 19:24:38 -0300

    se agregaron anotaciones de documentacion

--- src/main/java/com/example/Books/controllers/BookController.java
+++ src/main/java/com/example/Books/controllers/BookController.java
@@ -3,6 +3,12 @@ package com.example.Books.controllers;
 
 import com.example.Books.entities.Book;
 import com.example.Books.services.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,25 +22,47 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping
+    @Operation(summary = "Gets all books", description = "Gets all books.")
+    @ApiResponse(responseCode = "200",description = "List of all books", content = @Content(mediaType = "application/json",array = @ArraySchema(schema = @Schema(implementation = Book.class))))
+    @ApiResponse(responseCode = "400", description = "Request error")
+    @ApiResponse(responseCode = "500",description = "Internal server error")
     public List<Book> getAllBooks(){
         return bookService.getAllBooks();
     }
 
     @PostMapping
+    @Operation(summary = "Insert book",description = "Insert a new book")
+    @ApiResponse(responseCode = "200",description = "Book inserted succesfully", content = @Content(mediaType = "application/json",schema = @Schema(implementation = Book.class)))
+    @ApiResponse(responseCode = "400",description = "Request error")
+    @ApiResponse(responseCode = "500",description = "Internal server error")
     public Book addBook(@RequestBody @Valid Book book){
         return bookService.addBook(book);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Gets book by id",description = "Gets a book by id")
+    @ApiResponse(responseCode = "200",description = "Book found",content = @Content(mediaType = "application/json",schema = @Schema(implementation = Book.class)))
+    @ApiResponse(responseCode = "400",description = "Request error")
+    @ApiResponse(responseCode = "500",description = "Internal server error")
+    @Parameter(description = "ID of the book to retrieve", required = true, example = "1")
     public Book findBookById(@PathVariable Long id){
         return bookService.findBookById(id);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes a book by id",description = "Deletes a book by id.")
+    @ApiResponse(responseCode = "200",description = "Book deleted succesfully")
+    @ApiResponse(responseCode = "400",description = "Request error")
+    @ApiResponse(responseCode = "500",description = "Internal server error")
     public void deleteBook(@PathVariable Long id){
         bookService.deleteBook(id);
     }
+
     @PutMapping("/{id}")
+    @Operation(summary = "Updates a book by id",description = "Updates a book by id.")
+    @ApiResponse(responseCode = "200",description = "Book updated succesfully",content = @Content(mediaType = "application/json",schema = @Schema(implementation = Book.class)))
+    @ApiResponse(responseCode = "400",description = "Request error")
+    @ApiResponse(responseCode = "500",description = "Internal server error")
     public Book updateBook(@PathVariable Long id, @Valid @RequestBody Book book){
         return bookService.updateBook(id,book);
     }
