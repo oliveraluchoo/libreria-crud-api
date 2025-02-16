672f76fef637c0354626b524c9e3370f98a2e5c9
oliveraLuciano <lucianoolivera756@gmail.com>
2025-02-16 19:34:47 -0300

    se agregaron mas anotaciones de documentacion

--- src/main/java/com/example/Books/entities/Book.java
+++ src/main/java/com/example/Books/entities/Book.java
@@ -1,5 +1,6 @@
 package com.example.Books.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
@@ -22,17 +23,22 @@ public class Book {
     private Long id;
     @NotBlank(message = "ISBN cannot be blank")
     @NotNull(message = "ID cannot be null")
+    @Schema(description = "Book's isbn",example = "100")
     private String isbn;
     @NotBlank(message = "Title cannot be blank")
     @NotNull(message = "ID cannot be null")
+    @Schema(description = "Book's title",example = "Don Quijote")
     private String titulo;
     @NotBlank(message = "Author cannot be blank")
     @NotNull(message = "ID cannot be null")
+    @Schema(description = "Book's author",example = "Miguel de Cervantes")
     private String autor;
     @NotNull(message = "Publication year cannot be null")
+    @Schema(description = "Book's release year",example = "2024")
     private Integer aniopublicacion;
     @NotBlank(message = "Genre cannot be blank")
     @NotNull(message = "ID cannot be null")
+    @Schema(description = "Book's genre",example = "drama")
     private String genero;
 
 
