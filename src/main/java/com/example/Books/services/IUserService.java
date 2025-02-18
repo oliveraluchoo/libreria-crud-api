bdae1fbeb6eae1bd7289ffc27f0c45a850216488
oliveraLuciano <lucianoolivera756@gmail.com>
2025-02-18 11:25:55 -0300

    se arreglo interfaz IUserService

--- src/main/java/com/example/Books/services/IUserService.java
+++ src/main/java/com/example/Books/services/IUserService.java
@@ -1,5 +1,9 @@
 package com.example.Books.services;
 
-public interface IUserService {
+import com.example.Books.entities.User;
 
+public interface IUserService {
+    public User getUserById(Long id);
+    public User getUserByUsername(String username);
+    public User saveUser(User givenUser);
 }
